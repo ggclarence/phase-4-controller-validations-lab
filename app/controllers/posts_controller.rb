0fa6b7a79d614281426d92c4816d5e986c731086
Eric Van <ggclarence@yahoo.com>
2022-04-07 16:50:19 -0500

    commit tree 5f6a292c1119cc5936f27160f611cd06c43d8e44

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+
+
   def show
     post = Post.find(params[:id])
     
@@ -8,16 +10,21 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
+
+
 
   private
 
   def post_params
     params.permit(:category, :content, :title)
   end
+
+ 
+
 
 end
